c140e2da542658b9b065e20b2e372a0251e124a3
Thuc <huythuctran912@gmail.com>
2024-10-24 18:34:44 +0300

    updated

---
+++ snowpipe_streaming/scripts/execution/3_configure_environments.sh
@@ -0,0 +1,7 @@
+## Find the private endpoint for TLS authentication of the MSK cluster
+export BS=[cluster_private_endpoint]
+echo "export BS=$BS" >> ~/.bashrc
+
+## Create Kafka Topic
+$HOME/snowpipe-streaming/kafka_2.12-2.8.1/bin/kafka-topics.sh --bootstrap-server $BS --command-config $HOME/snowpipe-streaming/scripts/client.properties --create --topic trade_streaming --partitions 2 --replication-factor 2
+$HOME/snowpipe-streaming/kafka_2.12-2.8.1/bin/kafka-topics.sh --bootstrap-server $BS --command-config $HOME/snowpipe-streaming/scripts/client.properties --describe --topic streaming
